84b8a1229492a63d7de2b0c0b51a82368f0fe710
lucaspchartier <lucaspchartier@gmail.com>
2021-01-19 20:40:59 -0500

    chore(defer.go): add defer keyword in func main add makeThingY function to be called in func main

--- defer.go
+++ defer.go
@@ -2,11 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
-	deferTest()
+var thing string
 
+func main() {
+	thing = "x"
+	defer deferTest(makeThingY(thing))
+
+	fmt.Println("Good")
+	fmt.Println("Bye")
 }
 
-func deferTest() {
+func makeThingY(thing string) string {
+	thing = "y"
+	return "thing called"
+}
+
+func deferTest(thingReturn string) {
+	fmt.Println(thingReturn)
 	fmt.Println("Hi")
 }
