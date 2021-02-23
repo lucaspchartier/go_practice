3c320243f8cc9b32a9450d393b0148933b2f6750
lucaspchartier <lucaspchartier@gmail.com>
2021-02-22 19:21:19 -0500

    chore(main.go): rename func sum to func buy

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	sum(2, 2)
-	sum(1, 2, 3)
+	buy(2, 2)
+	buy(1, 2, 3)
 }
 
-func sum(...nums int) {
+func buy(...nums int) {
 	total := 0
 	for _, num := range nums {
 		total+=num
