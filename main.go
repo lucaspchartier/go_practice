6a0bde62fbd4644a310c3ad535cb1ba94075b1ae
lucaspchartier <lucaspchartier@gmail.com>
2021-02-22 19:47:29 -0500

    chore(main.go): print string after if statement to indicate the item and amount purchased in func buy

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	buy("milk", 3)
 	buy("cookies", 4, 1)
@@ -10,4 +12,6 @@ func buy(item string, amount int, discount ...int) {
 	if len(discount > 0) {
 		amount -= discount[0]
 	}
+
+	fmt.Printf("You bought %v for %d", amount)
 }
