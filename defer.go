710db06c7e46df6ab37e6ea9433fa4ec4197db88
lucaspchartier <lucaspchartier@gmail.com>
2021-02-24 20:21:21 -0500

    chore(main.go): comment out func main in defer.go

--- defer.go
+++ defer.go
@@ -1,21 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+// func main() {
+// 	defer deferTest(time.Now())
+// 	for i := 0; i < 10; i++ {
+// 		defer fmt.Println(i)
+// 	}
 
-func main() {
-	defer deferTest(time.Now())
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
-	}
+// 	fmt.Println("Good")
+// 	fmt.Println("Bye")
+// }
 
-	fmt.Println("Good")
-	fmt.Println("Bye")
-}
-
-func deferTest(start time.Time) {
-	elapsed := time.Since(start)
-	fmt.Println(elapsed)
-}
+// func deferTest(start time.Time) {
+// 	elapsed := time.Since(start)
+// 	fmt.Println(elapsed)
+// }
